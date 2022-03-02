f5cd3a0d28c1ba2a6bc5e5ddd8c92190232e4357
deveper <semsnt12@gmail.com>
2022-03-02 22:07:42 +0200

    RepositoriesAdded IGenericRepository

--- Customerize.Core/Entities/Category.cs
+++ Customerize.Core/Entities/Category.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Customerize.Core.Entities
+﻿namespace Customerize.Core.Entities
 {
     public class Category : BaseEntity
     {
