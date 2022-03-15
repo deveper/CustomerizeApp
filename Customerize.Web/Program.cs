241292aa442eaef37dd5cdde1d1dc2f10f098778
deveper <semsnt12@gmail.com>
2022-03-15 21:56:24 +0200

    AutoMapper service Added

--- Customerize.Web/Program.cs
+++ Customerize.Web/Program.cs
@@ -1,10 +1,25 @@
+using Customerize.Core.Repositories;
+using Customerize.Core.Services;
+using Customerize.Core.UnitOfWorks;
 using Customerize.Repository;
+using Customerize.Repository.Repostories;
+using Customerize.Service.Mapping;
+using Customerize.Service.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<IUnitOfWork, AppDbContext>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped(typeof(IService<>), typeof(Service<>));
+builder.Services.AddAutoMapper(typeof(MapProfile));
+
+
+
+
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"));
