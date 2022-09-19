df6ba9008bf94e21b37695bb92a620082f8ab0ea
deveper <semsnt12@gmail.com>
2022-09-19 18:55:52 +0300

    Advertisement Configuration

---
+++ Customerize.Repository/EntitiesConfigurations/AdvertisementConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Customerize.Core.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customerize.Repository.EntitiesConfigurations
+{
+    internal class AdvertisementConfiguration : IEntityTypeConfiguration<Advertisement>
+    {
+
+
+        public void Configure(EntityTypeBuilder<Advertisement> builder)
+        {
+            //appuser
+            builder.HasOne(ad => ad.AppUser)
+                .WithMany(ad => ad.Advertisements)
+                .HasForeignKey(ad => ad.UserId)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
