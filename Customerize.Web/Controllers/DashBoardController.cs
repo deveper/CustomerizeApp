814757911afd279232fd50a281e101b58f586daa
deveper <semsnt12@gmail.com>
2022-09-20 15:25:04 +0300

    DashBoard Controller and FE Changes

---
+++ Customerize.Web/Controllers/DashBoardController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Customerize.Web.Controllers
+{
+    public class DashBoardController : Controller
+    {
+
+        public DashBoardController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
