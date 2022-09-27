c2328a19f069838d5cf08c93847599f84adf36f0
deveper <semsnt12@gmail.com>
2022-09-27 23:21:35 +0300

    -

--- Customerize.Core/DTOs/OrderLine/OrderLineDtoInsert.cs
+++ Customerize.Core/DTOs/OrderLine/OrderLineDtoInsert.cs
@@ -3,7 +3,6 @@
     public class OrderLineDtoInsert : BaseDto
     {
         public int OrderId { get; set; }
-
         public int ProductId { get; set; }
         public int ProductPiece { get; set; }
     }
