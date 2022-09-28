0bcec23044a8868176738af19ec4ad09ae267ea7
deveper <semsnt12@gmail.com>
2022-09-28 18:00:46 +0300

    OrederStatuses and OrderStatus for Order Entity

---
+++ Common/StaticClasses/OrderStatuses.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customerize.Common.StaticClasses
+{
+    public class OrderStatuses
+    {
+        public const long İşlemdeki = 1;
+        public const long Beklemede = 2;
+        public const long Hazır = 3;
+        public const long Dağıtımda = 4;
+
+    }
+}
