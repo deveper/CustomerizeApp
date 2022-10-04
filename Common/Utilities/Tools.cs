30afe21b4ba3d7c520866c869a603e51cde74f8c
deveper <semsnt12@gmail.com>
2022-10-04 16:31:23 +0300

    add  Tools for Common

---
+++ Common/Utilities/Tools.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customerize.Common.Utilities
+{
+    public class Tools
+    {
+
+        public decimal TotalAmount()
+        {
+            return 0;
+        }
+    }
+}
+
