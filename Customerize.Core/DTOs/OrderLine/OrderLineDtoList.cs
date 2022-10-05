503d834ac93c5c2feeb0efb9a130c99a326aa15b
deveper <semsnt12@gmail.com>
2022-10-05 17:21:04 +0300

    OrderDetailPage LineAmount

--- Customerize.Core/DTOs/OrderLine/OrderLineDtoList.cs
+++ Customerize.Core/DTOs/OrderLine/OrderLineDtoList.cs
@@ -6,6 +6,7 @@
         public int ProductId { get; set; }
         public string ProductName { get; set; }
         public int ProductPiece { get; set; }
+        public decimal? LineAmount { get; set; }
 
     }
 }
